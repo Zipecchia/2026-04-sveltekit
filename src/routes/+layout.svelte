c09bf1e4058b2f920b1077f7fab2e4bb115f1308
Zipecchia <viola.naldi5@gmail.com>
2026-04-27 16:23:48 +0200

    Fon addition APi 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,8 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-<!--Font georama-->
+
+	<!--Font Georama-->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Georama:ital,wght@0,100..900;1,100..900&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
